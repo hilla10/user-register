6e5c8818519e280fc46eb65467182ca570eed921
hilla <hillaman592@gmail.com>
2025-03-31 19:51:26 +0300

    Update schema.sql

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,13 +1,16 @@
 --creating table
 
-
-/*# multi line comment
-*/
 CREATE TABLE IF NOT EXISTS users (
     id VARCHAR(50) PRIMARY KEY NOT NULL,
     username VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     role ENUM('user', 'admin') DEFAULT 'user',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS profile_pictures (
+    id VARCHAR(50) PRIMARY KEY NOT NULL,
+    file_path VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
